1e6f521dd8b09b9fb5b1a26ec0bd3a526e9de2bf
s729492 <s729492@dnata.com>
2017-07-15 19:31:00 +0400

    initial version details

---
+++ src/main/java/com/mcan/McanApplication.java
@@ -0,0 +1,12 @@
+package com.mcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McanApplication.class, args);
+	}
+}
